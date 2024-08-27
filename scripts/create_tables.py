b1f7eaf5db1b9637d6cfa2dbf9f3b355ce6b9e28
Darius Pop-Tarau <84680812+dariuspt@users.noreply.github.com>
2024-08-27 03:46:57 +0300

    Delete scripts directory

--- scripts/create_tables.py
+++
@@ -1,6 +0,0 @@
-# scripts/create_tables.py
-
-from db.tables import create_tables
-
-if __name__ == "__main__":
-    create_tables()
--- scripts/insert_data.py
+++
@@ -1,6 +0,0 @@
-# scripts/insert_data.py
-
-from db.data import insert_data
-
-if __name__ == "__main__":
-    insert_data()
